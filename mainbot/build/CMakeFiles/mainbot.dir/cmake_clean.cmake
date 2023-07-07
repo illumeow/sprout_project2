c43a4c94c04aa54e1aed9f4ea2d9610f4b075095
illumeow <illumeow0223@gmail.com>
2023-07-07 12:57:24 +0800

    add mainbot/build/

---
+++ mainbot/build/CMakeFiles/mainbot.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mainbot.dir/src/main.cpp.o"
+  "mainbot"
+  "mainbot.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mainbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
